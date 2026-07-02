460c2b1aa101c5d86f666f9a83d66d0073eb5a57
mateusaa <mateus.a.araujo2006@gmail.com>
2026-07-01 21:34:03 -0300

    trabalhos feito dia 01/07

---
+++ Assets/Scripts/ButtonAction.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class ButtonAction : MonoBehaviour
+{
+    public void OnButtonClick()
+    {
+        Debug.Log("Hello World!");
+        // Add your button click logic here
+    }
+}
